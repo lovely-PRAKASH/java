97766af54ad752594bf6244b0198747beb4e0629
prakash-2002 <prakashslm27@gmail.com>
2024-12-29 21:56:34 +0530

    bus main file

---
+++ JavaPratice/BusReservation/BusDemo.java
@@ -0,0 +1,36 @@
+package JavaPratice.BusReservation;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class BusDemo {
+    public static void main(String[] args) {
+        
+        ArrayList<Bus> buses=new ArrayList<Bus>(); 
+        ArrayList<Booking> bookings=new ArrayList<Booking>();
+        buses.add(new Bus(1, true,2));
+        buses.add(new Bus(2, false, 30));
+        buses.add((new Bus(3, true, 40)));
+
+        for(Bus b:buses){
+            b.displayDetials();
+        }
+        int choice=1;
+        System.out.println("Welcome to Bus Reservation");
+        Scanner scn= new Scanner(System.in);
+
+        while(choice==1){
+            System.out.println("choose 1. for bus booking & choose 2. for exit");
+            choice=scn.nextInt();
+            if(choice==1){
+                System.out.println("booking");
+                Booking booking = new Booking();
+                if(booking.isAvailable(bookings, buses)){
+                    bookings.add(booking);
+                }else{
+                    System.out.println("booking is full try another bus or date");
+                }
+            }else{
+                System.out.println("bus res is cancelled");
+            }
+        }
+    }
+}
