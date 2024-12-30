fe380d4af68d113053d3bdeb7e82b5ccfa2042e5
prakash-2002 <prakashslm27@gmail.com>
2024-12-30 20:55:41 +0530

    constructor added

--- JavaPratice/abstractF/abstractDemo.java
+++ JavaPratice/abstractF/abstractDemo.java
@@ -3,6 +3,10 @@ package JavaPratice.abstractF;
 abstract class Member{
    private String name;
 
+   Member(String n){
+    this.name=n;
+   }
+
     abstract void greetings();
 
     public String getName(){
@@ -10,13 +14,20 @@ abstract class Member{
     }
 }
 class Student extends Member{
- 
+
+    Student(String n){
+        super(n);
+    }
+
     void greetings(){
         System.out.println("Welcome student...");
     }
 }
 class Teacher extends Member{
 
+    Teacher(String n){
+        super(n);
+    }
     void greetings(){
         System.out.println("welcome Teacher...");
     }
@@ -26,12 +37,14 @@ public class abstractDemo {
     public static void main(String[] args) {
 
         Member[] m1=new Member[4];
-        m1[0]=new Student();
-        m1[1]= new Student();
-        m1[2]=new Teacher();
+        m1[0]=new Student("student1");
+        m1[1]= new Student("student2");
+        m1[2]=new Teacher("teacher1");
+        m1[3]=new Teacher("teacher2");
 
         for (Member m:m1){
             m.greetings();
+            System.out.println(m.getName());;
         }
         
     }
