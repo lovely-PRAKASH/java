fd6e382b3bf9e920c71d21a84e8040aec60a9ee5
prakash-2002 <prakashslm27@gmail.com>
2024-12-30 21:42:10 +0530

    constructor added

--- JavaPratice/abstractF/abstractDemo2.java
+++ JavaPratice/abstractF/abstractDemo2.java
@@ -1,26 +1,36 @@
 package JavaPratice.abstractF;
 
 abstract class Shape{
+    
     abstract void area();
-
 }
 class Square extends Shape{
+    private double side;
+    
+    public Square(double s){
+        this.side = s;
+    };
+
     void area(){
-        System.out.println("area of square");
+        System.out.println("area of square is "+side*side);
     }
 }
 class Triangle extends Shape{
-    
+    private double height;
+    private double base;
+    Triangle(double h, double b){
+        this.height=h;
+        this.base=b;
+    }
     void area() {
-        // TODO Auto-generated method stub
-        System.out.println("area of triangle");
+        System.out.println("area of triangle is "+ height*base);
     }
 }
 public class abstractDemo2 {
     public static void main(String[] args) {
         Shape[] shapes=new Shape[2];
-        shapes[0]=new Square();
-        shapes[1]=new Triangle();
+        shapes[0]=new Square(2);
+        shapes[1]=new Triangle(4,2);
 
         for(Shape s:shapes){
             s.area();
