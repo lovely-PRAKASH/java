19af4d65a51cdadd72d8150f70873c076f713ee1
prakash-2002 <prakashslm27@gmail.com>
2025-01-01 16:57:12 +0530

    multiple interface added

--- JavaPratice/interfacePratice/InterfaceDemo.java
+++ JavaPratice/interfacePratice/InterfaceDemo.java
@@ -12,8 +12,16 @@ class Car implements Ridable{
     public void display(){
         System.out.println("your are riding a car1");
     }
+    public void checkCar(){
+        System.out.println("calling car using bike");
+    }
 }
-class Bike implements Ridable{
+interface Rolles{
+    default public void carType(){
+        System.out.println("Rolles Royes car");
+    }
+}
+class Bike extends Car implements Ridable, Rolles{
     public void ride(){
         System.out.println("Your are Riding a Bike");
     }
@@ -26,10 +34,13 @@ public class InterfaceDemo{
     public static void main(String[] args) {
         Ridable c=new Car();
         Ridable b= new Bike();
+        Bike b1=new Bike();
 
         c.ride();
         b.ride();
         c.display();
         b.display();
+        b1.checkCar();
+        b1.carType();
     }
 }
