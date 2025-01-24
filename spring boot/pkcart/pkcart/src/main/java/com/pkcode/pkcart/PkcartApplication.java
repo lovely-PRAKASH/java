db2980c2ca996702812588cd4cb93134e0f2e699
prakash-2002 <prakashslm27@gmail.com>
2025-01-24 19:05:09 +0530

    main class file

---
+++ spring boot/pkcart/pkcart/src/main/java/com/pkcode/pkcart/PkcartApplication.java
@@ -0,0 +1,13 @@
+package com.pkcode.pkcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkcartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkcartApplication.class, args);
+	}
+
+}
